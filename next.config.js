4229a0acc99853bc9d059aa74d6268eb1d3267bb
Asaf Shefer <asaf6f@gmail.com>
2023-09-26 13:04:35 +0300

    implementing mdx editor

--- next.config.js
+++ next.config.js
@@ -3,11 +3,30 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra({
-  // ! deprecated for now
-  //! i18n: {
-  //!   locales: ['he'],
-  //!   defaultLocale: 'he',
-  //! },
+const nextConfig = {
+  transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
   poweredByHeader: false,
-})
+}
+
+module.exports = withNextra(nextConfig)
+
+
+
+
+
+
+
+
+// const withNextra = require('nextra')({
+//   theme: 'nextra-theme-docs',
+//   themeConfig: './theme.config.tsx',
+// })
+
+// module.exports = withNextra({
+//   poweredByHeader: false,
+// })
