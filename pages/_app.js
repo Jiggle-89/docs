53a3b5e8367a0607b2c4642585ee5db97a90f6fa
Asaf Shefer <asaf6f@gmail.com>
2023-11-16 17:32:19 +0200

    added custom edit page button for new site

--- pages/_app.js
+++ pages/_app.js
@@ -2,3 +2,8 @@
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+export function EditPage() {
+  return (
+    <a style={{'position': 'absolute','left': '23.5%' ,'marginTop': '30px'}} href="/" target="_blank" rel="noreferrer noopener">ערוך דף</a>
+  )
+}
