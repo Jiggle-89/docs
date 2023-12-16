a647b86e746fd2acfb0354ec178bb6249860ba4d
asaf6f <asaf6f@gmail.com>
2023-12-16 15:03:29 +0200

    trying to implement fix for safari

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+import MyComponent from "../public/stay_standalone"
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MyComponent />
+      <Component {...pageProps} />
+    </>
+  )
+
 }
