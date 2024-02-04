593cbe64153cc9e3c893b801dbacd20b956f2f25
Asaf Shefer <asaf6f@gmail.com>
2024-02-04 15:27:26 +0200

    changing ---title--- frontmatter to _meta.json titles

--- next.config.js
+++ next.config.js
@@ -16,11 +16,9 @@
 
 // module.exports = withNextra(nextConfig)
 
-
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.tsx'
 })
 
 module.exports = withNextra({
