e3f95fa8e830d30c0e83df852fdae78e984acdc3
Asaf Shefer <asaf6f@gmail.com>
2024-06-04 13:13:22 +0300

    adding vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 // This default export is required in a new `pages/_app.js` file.
 import '../components/ckeditor.css' // ckeditor css styles
+import { Analytics } from "@vercel/analytics/react"
+
 export default function MyApp({ Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  )
 }
